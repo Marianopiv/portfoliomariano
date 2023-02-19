5315d910764a444b28a5a9bb7c89b97e73e679db
Marianopiv <marianopividori93@gmail.com>
2023-02-19 19:27:59 -0300

    agrego luces a botones y hover

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({action,text,className}) => {
   return (
-    <div onClick={action} className={`btn-class  w-36 h-18 pb-8 text-xs text-white ${className?className:""}`}>{text}</div>
+    <div onClick={action} className={`btn-class  w-36 h-18 pb-8 text-xs text-white  ${className?className:""}`}>{text}</div>
   )
 }
 
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -47,7 +47,7 @@ const NavBar = () => {
         </div>
         <div className="sm:flex gap-4 justify-center w-1/3 py-2 hidden ">
           {fullRedes&&fullRedes.map(({img,link}, index) => (
-            <div  key={index} className="" onClick={()=>openLinkInNewWindow(link)}>
+            <div  key={index} className="hover:cursor-pointer" onClick={()=>openLinkInNewWindow(link)}>
             <img  className="w-6 h-6" src={img} alt="" />
           </div>
           ))}
