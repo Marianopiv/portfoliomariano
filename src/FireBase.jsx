055e1a588213f61c8031fa6dc71be3ca59c4a157
Marianopiv <marianopividori93@gmail.com>
2023-02-28 15:26:41 -0300

    saco funcion inecesaria

--- src/FireBase.jsx
+++ src/FireBase.jsx
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
-import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +17,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const database = getDatabase(app);
+
+
+console.log(firebaseConfig.projectId);
+console.log(firebaseConfig.apiKey);
+console.log(firebaseConfig.authDomain);
+console.log(firebaseConfig.storageBucket);
+console.log(firebaseConfig.messagingSenderId);
+console.log(firebaseConfig.appId);
